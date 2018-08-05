28dc864286ac201e566ffd367c5134c40a69bf49
Teng Han <hanteng1021@outlook.com>
2018-08-05 15:15:19 +0800

    test branch

--- js/testtest.js
+++ js/testtest.js
@@ -1,14 +1,14 @@
 function testtest(){
 
-	var vector = new THREE.Vector3(40, 0, 0);
+	// var vector = new THREE.Vector3(40, 0, 0);
 
-	var ref = new THREE.Vector3(0, 1, 0);
+	// var ref = new THREE.Vector3(0, 1, 0);
 
-	for(var i = 0; i < 6; i++) {
-		vector.applyAxisAngle(ref, -30  * Math.PI / 180);
+	// for(var i = 0; i < 6; i++) {
+	// 	vector.applyAxisAngle(ref, -30  * Math.PI / 180);
 
-		console.log(vector);
-	}
+	// 	console.log(vector);
+	// }
 
 	
 
