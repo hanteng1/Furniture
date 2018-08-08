6ee5d3d6c5ae3c629e206400c4033a695a981fbb
Triftw <34067626+Triftw@users.noreply.github.com>
2018-08-09 02:15:55 +0800

    putwall
    still trying csg

--- js/Chair_Putwall.js
+++ js/Chair_Putwall.js
@@ -2,14 +2,89 @@
 
 
 function Chair_Putwall (main) {
+	this.main = main;
+	this.furnitures = main.furnitures;
 
+	this.reference = null;  //midframe, seat, mixed
+
+
+	this.parameters = {
+
+		HEIGHT: 30,
+		ANGLE: 0
+
+	};
+
+	//number of furnitures
 
 }
 
 
 Chair_Putwall.prototype = {
 
+	checkHasFrame: function(furniture) {
+		
+		return furniture.hasComponent('midframe');
+	},
 
+
+	checkHasSeat: function(furniture) {
+		
+		return furniture.hasComponent('seat');
+	},
+
+	checkHasBack: function(furniture) {
+		
+		return furniture.hasComponent('back');
+	},
+
+	getFrameHeight2Floor: function(furniture) {
+		return furniture.getComponentHeight2Floor('midframe');
+	},
+
+	getSeatHeight2Floor: function(furniture) {
+		return furniture.getComponentHeight2Floor('seat');
+	},
+
+	checkLabeledComponents: function(furnitures) {
+
+		count = furnitures.length;
+		//console.log(`number of chairs: ${count}`);
+
+		//determine reference type
+		var hasFrameCount = 0;
+		var hasSeatCount = 0;
+		for(var i = 0; i < count; i++)
+		{
+			if(this.checkHasFrame(furnitures[i])) hasFrameCount++;
+			if(this.checkHasSeat(furnitures[i])) hasSeatCount++;	
+		}
+
+		if(hasFrameCount == count) {
+			this.reference = 'midframe';
+
+		}else if(hasSeatCount == count) {
+			this.reference = 'seat';
+		}else {
+			//don't handle for now
+			console.log("frame count or seat count not matched");
+		}
+
+
+		//todo: check the height and see whether they are the same
+		if(this.reference == 'midframe') {
+			//check the height
+
+		}
+
+	},
+
+	//////////////////////////////////////////////////////////////////////////
+	//make transformation
+
+	execute: function(){
+		this.putwall(this.furnitures, this.parameters.DISTANCE, this.parameters.ANGLE);
+	},
 
 
 }
